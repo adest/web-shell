4a6008314e0b06dfd8ed925bde34a3a92f068ece
adrien.estran <adrien.estran@decisionbrain.com>
2015-03-29 14:39:07 +0200

    use tsd to install typing and convert protractor test to typescript.

--- test/e2e-spec/GoogleSpec.ts
+++ test/e2e-spec/GoogleSpec.ts
@@ -1,14 +1,17 @@
 /**
  * Created by Adrien on 29/03/2015.
  */
+
 /// <reference path="../../typings/tsd.d.ts"/>
-describe('Google web site', function () {
-    beforeEach(function () {
+
+describe('Google web site', () => {
+
+    beforeEach(() => {
         browser.ignoreSynchronization = true;
         browser.get('http://www.google.fr');
-    });
-    it('is titled with Google', function () {
+    })
+
+    it('is titled with Google', () => {
         expect(browser.getTitle()).toBe('Google');
     });
 });
-//# sourceMappingURL=GoogleSpec.js.map
